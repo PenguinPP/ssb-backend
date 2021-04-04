862ccd35aafffec93d6018b73c1dc78caee790d0
Paldeep <penguinpp93@gmail.com>
2021-04-04 17:13:23 +1000

    errors and swagger definitions

--- ssb-app-server/src/daos/Recipe/RecipeDao.ts
+++ ssb-app-server/src/daos/Recipe/RecipeDao.ts
@@ -1,7 +1,7 @@
 import Recipe, { IRecipe } from "@entities/Recipe";
-import RecipePreview, {IRecipePreview} from "@entities/RecipePreview"
+import RecipePreview, { IRecipePreview } from "@entities/RecipePreview";
 import neo4j = require("neo4j-driver");
-import neo4jDriver from "@daos/SsbDB/Neo4jDriverDao"
+import neo4jDriver from "@daos/SsbDB/Neo4jDriverDao";
 
 export interface IRecipeDao {
   getAll: () => Promise<IRecipe[]>;
@@ -51,66 +51,82 @@ class RecipeDao implements IRecipeDao {
   public async getAllPreviews(): Promise<IRecipePreview[]> {
     let session = neo4jDriver.session();
 
-    let result = await session
-        .run(
-            `MATCH (r:recipe)-[:HAS_MAIN_INGREDIENT]->(m:ingredient)
+    let result = await session.run(
+      `MATCH (r:recipe)-[:HAS_MAIN_INGREDIENT]->(m:ingredient)
             WITH collect(m.name) AS main_ingredients, r
             MATCH (r)-[:HAS_TAG]->(t:tag)
             WITH collect(t.name) AS recipe_tags, r, main_ingredients
             RETURN r.name AS recipe_name, r.recipeId AS recipe_id, recipe_tags, main_ingredients;`
-        )
+    );
 
     session.close();
-    
+
     return result.records.map((record) => {
-        const {recipe_name, recipe_id, recipe_tags, main_ingredients} = record.toObject()
+      const {
+        recipe_name,
+        recipe_id,
+        recipe_tags,
+        main_ingredients,
+      } = record.toObject();
 
-        const recipe_preview : IRecipePreview = {
-            recipe_name: recipe_name,
-            recipe_id: recipe_id,
-            recipe_tags: recipe_tags,
-            main_ingredients: main_ingredients
-        }
+      const recipe_preview: IRecipePreview = {
+        recipe_name: recipe_name,
+        recipe_id: recipe_id,
+        recipe_tags: recipe_tags,
+        main_ingredients: main_ingredients,
+      };
 
-        return recipe_preview;
-    })
-
+      return recipe_preview;
+    });
   }
 
   public async getRecipeById(recipeId: number): Promise<IRecipe> {
     let session = neo4jDriver.session();
-    let result = await session
-        .run(
-            `MATCH (r:recipe)-[:HAS_MAIN_INGREDIENT]->(m:ingredient)
+
+    try {
+      let result = await session.run(
+        `MATCH (r:recipe)-[:HAS_MAIN_INGREDIENT]->(m:ingredient)
             WHERE r.recipeId = $selectedRecipeId 
             WITH collect(m.name) AS main_ingredients, r
             MATCH (r)-[c:CONTAINS_INGREDIENT]->(i:ingredient)
             WITH collect(i.name) AS all_ingredients, r, main_ingredients, collect(c.amount) AS ingredient_amounts, collect(c.unit) AS ingredient_units, collect (c.preparation) AS ingredient_prep
             RETURN r.recipeId AS recipe_id, r.name AS recipe_name, main_ingredients, all_ingredients, ingredient_amounts, ingredient_units, ingredient_prep;`,
-            {selectedRecipeId : recipeId}
-        )
-    session.close();
+        { selectedRecipeId: recipeId }
+      );
 
-    const record = result.records[0]
-    const {recipe_id, recipe_name, recipe_picture, all_ingredients, main_ingredients, ingredient_amounts, ingredient_prep, ingredient_units} = record.toObject();
+      if (result.records.length == 0) {
+        throw new Error("recipeId provided does not exist.");
+      }
+      const record = result.records[0];
+      const {
+        recipe_id,
+        recipe_name,
+        recipe_picture,
+        all_ingredients,
+        main_ingredients,
+        ingredient_amounts,
+        ingredient_prep,
+        ingredient_units,
+      } = record.toObject();
 
-    const recipe : IRecipe = {
-            recipe_id: recipe_id,
-            recipe_name: recipe_name,
-            recipe_picture: recipe_picture,
-            all_ingredients: all_ingredients,
-            main_ingredients: main_ingredients,
-            ingredient_amounts: ingredient_amounts,
-            ingredient_units: ingredient_units,
-            ingredient_prep: ingredient_prep
-        }
+      const recipe: IRecipe = {
+        recipe_id: recipe_id,
+        recipe_name: recipe_name,
+        recipe_picture: recipe_picture,
+        all_ingredients: all_ingredients,
+        main_ingredients: main_ingredients,
+        ingredient_amounts: ingredient_amounts,
+        ingredient_units: ingredient_units,
+        ingredient_prep: ingredient_prep,
+      };
 
-    return recipe
-
+      return Promise.resolve(recipe);
+    } catch (error) {
+      throw error;
+    } finally {
+      session.close();
+    }
   }
-
 }
-
-
 
 export default RecipeDao;
--- ssb-app-server/src/routes/Recipes.ts
+++ ssb-app-server/src/routes/Recipes.ts
@@ -2,11 +2,11 @@ import StatusCodes from "http-status-codes";
 import { Request, Response, Router } from "express";
 import RecipeDao from "@daos/Recipe/RecipeDao";
 import { paramMissingError, IRequest } from "@shared/constants";
-import { param } from 'express-validator';
+import { param } from "express-validator";
 
 const router = Router();
 const recipeDao = new RecipeDao();
-const { BAD_REQUEST, CREATED, OK } = StatusCodes;
+const { BAD_REQUEST, CREATED, OK, NOT_FOUND } = StatusCodes;
 
 /******************************************************************************
  *                      Get All Recipes - "GET /api/recipes/all"
@@ -21,7 +21,7 @@ router.get("/all", async (req: Request, res: Response) => {
  *                      Get All Recipes Previews - "GET /api/recipes/previews"
  ******************************************************************************/
 
- router.get("/previews", async (req: Request, res: Response) => {
+router.get("/previews", async (req: Request, res: Response) => {
   const recipes = await recipeDao.getAllPreviews();
   return res.status(OK).json({ recipes });
 });
@@ -30,11 +30,20 @@ router.get("/all", async (req: Request, res: Response) => {
  *                      Get Recipe By Id - "GET /api/recipes/:recipeId"
  ******************************************************************************/
 
- router.get("/:recipeId", [param("recipeId").not().isEmpty()], async (req: Request, res: Response) => {
-  const recipes = await recipeDao.getRecipeById(parseInt(req.params.recipeId));
-  return res.status(OK).json({ recipes });
-});
-
-
+router.get(
+  "/:recipeId",
+  [param("recipeId").not().isEmpty()],
+  async (req: Request, res: Response) => {
+    try {
+      const recipes = await recipeDao.getRecipeById(
+        parseInt(req.params.recipeId)
+      );
+      return res.status(OK).json({ recipes });
+    } catch (error) {
+      console.log("Failure at recipe controller");
+      return res.status(BAD_REQUEST).json({ error: error.message });
+    }
+  }
+);
 
 export default router;
