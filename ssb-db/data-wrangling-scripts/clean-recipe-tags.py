c4afd4fe4738baaf6c8531cc0eb3756ad6f955dc
Paldeep <penguinpp93@gmail.com>
2021-04-10 14:23:38 +1000

    encode recipes to utf-8 and clean tags

--- ssb-db/data-wrangling-scripts/clean-recipe-tags.py
+++ ssb-db/data-wrangling-scripts/clean-recipe-tags.py
@@ -3,7 +3,7 @@ import csv
 try:
     tag_list = csv.DictReader(open('../data/tag-categories.csv'))
     recipe_list = csv.DictReader(
-        open('../data/cleaned-recipes.csv', encoding="UTF-8"))
+        open('../data/cleaned-recipes-utf-8.csv', encoding="utf-8-sig"))
 
     dish_name_change_list = csv.DictReader(
         open('../data/dish-name-changes.csv'))
@@ -18,10 +18,44 @@ try:
     unwanted_tag_categories = ["protein", "carb",
                                "vegetable and fruit", "dairy", "pantry"]
 
-    i = 0
+    unwanted_tags = []
+
+    cleaned_recipe_list = []
+
+    for tag in tag_list:
+        for category in unwanted_tag_categories:
+            if tag[category]:
+                unwanted_tags.append(tag[category])
+
+    tags_removed = 0
+    tags_name_changed = 0
 
     for recipe in recipe_list:
-        print(recipe["idMeal"])
+        current_tag_list = recipe["tags"].split(',')
+        new_tag_list = []
+        for index, recipe_tag in enumerate(current_tag_list):
+
+            # change tag names that need changing
+            if recipe_tag in old_dish_names:
+                current_tag_list[index] = new_dish_names[old_dish_names.index(
+                    recipe_tag)]
+                tags_name_changed += 1
+
+            # remove unwanted tags
+            if recipe_tag not in unwanted_tags:
+                new_tag_list.append(recipe_tag)
+
+        # replace old tag list with new tag list
+        recipe["tags"] = str(new_tag_list)
+
+        cleaned_recipe_list.append(recipe)
+    print(tags_removed, tags_name_changed)
+
+    with open('../data/cleaned-recipes-utf-8-v2.csv', 'w', encoding='utf-8-sig', newline='') as output_file:
+        dict_writer = csv.DictWriter(
+            output_file, cleaned_recipe_list[0].keys())
+        dict_writer.writeheader()
+        dict_writer.writerows(cleaned_recipe_list)
 
 except UnicodeDecodeError as e:
     offending = e.object[e.start:e.end]
